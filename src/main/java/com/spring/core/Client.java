9125db20bc0323296019d71e32285d65309ad277
Neeraj Kumar <68942993+Neeraj-Kumar-Coder@users.noreply.github.com>
2023-05-23 22:37:53 +0530

    Learned about JAVA BASE CONFIGURATION (using @configuration and @Bean annotations) in spring

--- src/main/java/com/spring/core/Client.java
+++ src/main/java/com/spring/core/Client.java
@@ -1,12 +1,30 @@
 package com.spring.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Client {
     public static void main(String[] args) {
 //        TightlyCoupledTraveller tightlyCoupledTraveller = new TightlyCoupledTraveller();
 //        tightlyCoupledTraveller.startJourney();
 
-        Vehicle vehicle = new Cycle();
-        LooselyCoupledTraveller looselyCoupledTraveller = new LooselyCoupledTraveller(vehicle);
+//        Vehicle vehicle = new Cycle();
+//        LooselyCoupledTraveller looselyCoupledTraveller = new LooselyCoupledTraveller(vehicle);
+//        looselyCoupledTraveller.startJourney();
+
+//        Creating the spring IoC container
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Car car = applicationContext.getBean(Car.class);
+        car.move();
+
+        Cycle cycle = applicationContext.getBean(Cycle.class);
+        cycle.move();
+
+        Bike bike = applicationContext.getBean(Bike.class);
+        bike.move();
+
+        LooselyCoupledTraveller looselyCoupledTraveller = applicationContext.getBean(LooselyCoupledTraveller.class);
         looselyCoupledTraveller.startJourney();
     }
 }
