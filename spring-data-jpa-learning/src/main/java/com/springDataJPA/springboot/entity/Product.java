6fecdc96721824ab2102201b18d1caad0159358d
Neeraj Kumar <68942993+Neeraj-Kumar-Coder@users.noreply.github.com>
2023-05-28 19:07:29 +0530

    Added more notes of spring and spring boot, also learned how to connect to MySQL database using ORM, JPA, Hibernate and Spring data JPA. Learned some basic annotations to create an entity and configure the table

---
+++ spring-data-jpa-learning/src/main/java/com/springDataJPA/springboot/entity/Product.java
@@ -0,0 +1,108 @@
+package com.springDataJPA.springboot.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "products",
+        schema = "ecommerce",
+        uniqueConstraints = @UniqueConstraint(name = "sku_unique", columnNames = "stock_keeping_unit")
+)
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "stock_keeping_unit", nullable = false)
+    private String sku;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private boolean active;
+
+    private String imageUrl;
+
+    private LocalDateTime creationDate;
+
+    private LocalDateTime lastUpdated;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(LocalDateTime lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+}
